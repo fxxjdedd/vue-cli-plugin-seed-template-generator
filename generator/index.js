7252d5fe675abf3c37998bd8ae54fe8b8e27c5c0
futengda <fxxjdedd@gmail.com>
2020-06-01 16:04:35 +0800

    feat: fix conflicts between custom generators and vue-cli offical generator

--- generator/index.js
+++ generator/index.js
@@ -31,6 +31,19 @@ function replaceDotFilePrefix() {
   })
 }
 
+// 如何判断一个文件是否是vue-cli生成的，我们基于以下3点：
+// 1. vue-cli的bare模式src中只会生成: App.vue, main.js, assets
+// 2. vue-cli的generator会在所有generator之前执行，所以如果有重名文件会被自定义的generator覆盖，不存在安全问题
+// 3. assets是所有技术栈通用的，而App.vue和main.js不是，所以我们只需要针对这两个文件判断
+function removeVueCliGeneratedFiles(files) {
+  ;['src/App.vue', 'src/main.js'].forEach(file => {
+    const fileExist = fs.existsSync(path.resolve(__dirname, 'template', file))
+    if (!fileExist) {
+      delete files[file]
+    }
+  })
+}
+
 module.exports = (api, options = {}) => {
   const { url, branch } = options
   if (!url) {
@@ -47,7 +60,25 @@ module.exports = (api, options = {}) => {
   // https://github.com/vuejs/vue-cli/pull/5537, When this pr is passed, this line can be deleted
   replaceDotFilePrefix()
 
+  const seedTemplatePackage = JSON.parse(
+    fs.readFileSync(path.resolve(templateDir, 'package.json'), 'utf-8'),
+  )
+
+  api.extendPackage(seedTemplatePackage)
+
   api.render('./template', {
     ...options,
   })
+
+  api.postProcessFiles(files => {
+    // 另外一个问题，即使是bare模式，vue-cli依然会生成一些文件，我们要把它删掉，保证生成的文件都是来自于种子仓库
+    removeVueCliGeneratedFiles(files)
+  })
 }
+
+// 关于这个插件与vue-cli自带的generator之间可能存在的冲突
+// vue-cli自带的generator会在所有自定义generator之前执行，所以一般不会存在冲突。但是依然会存在一些比较特殊的文件，可能会存在冲突：
+
+// README: 最新版的vue-cli会判断是否已经存在README，如果存在则不会进行覆盖
+// package.json: 如果想保留种子仓库中的package.json，则必须把它填充到extendPackage中。否则，vue-cli的package.json会完全覆盖种子仓库的.
+// 其他文件: 安全的
